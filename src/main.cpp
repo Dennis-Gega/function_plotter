35a9fae79cb37cd3ca713343ddd77b56607d8a14
dennis gega <dennis.gega@uconn.edu>
2026-01-13 17:47:08 -0500

    x and y axis on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
+#include "tinyexpr.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+void drawGrid(float width, float height, sf::RenderWindow& window) {
+    std::array x_axis =
+    {
+      sf::Vertex{sf::Vector2f(0, height / 2.0f)},
+      sf::Vertex{sf::Vector2f(width, height / 2.0f)},
+    };
+
+    std::array y_axis =
+    {
+      sf::Vertex{sf::Vector2f(width / 2.0f, 0)},
+      sf::Vertex{sf::Vector2f(width / 2.0f, width)},
+    };
+
+    window.draw(x_axis.data(), x_axis.size(), sf::PrimitiveType::Lines);
+    window.draw(y_axis.data(), y_axis.size(), sf::PrimitiveType::Lines);
+}
+
 int main()
 {
+
   auto window = sf::RenderWindow(sf::VideoMode({500u, 500u}), "function plotter");
   window.setFramerateLimit(144);
+
+  sf::Vector2u size = window.getSize();
+  auto [width, height] = size;
 
   while (window.isOpen())
   {
@@ -18,23 +40,7 @@ int main()
     }
 
     window.clear();
-
-    std::array x_axis =
-    {
-      sf::Vertex{sf::Vector2f(250.f, 0.f)},
-      sf::Vertex{sf::Vector2f(250.f, 500.f)}
-    };
-
-    window.draw(x_axis.data(), x_axis.size(), sf::PrimitiveType::Lines);
-    
-    std::array y_axis =
-    {
-      sf::Vertex{sf::Vector2f(0.f, 250.f)},
-      sf::Vertex{sf::Vector2f(500.f, 250.f)}
-    };
-
-    window.draw(y_axis.data(), y_axis.size(), sf::PrimitiveType::Lines);
-
+    drawGrid(width, height, window);
     window.display();
   }
 }
